837e3fcfb363e742c20bf740d4a40e4d00fc22f7
jeevanteja1304 <160091584+jeevanteja1304@users.noreply.github.com>
2025-08-31 07:35:10 +0530

    Update signup.js

--- signup.js
+++ signup.js
@@ -7,7 +7,7 @@ const genderInput = document.getElementById('gender');
 const passwordInput = document.getElementById('password');
 const errorMessage = document.getElementById('error-message');
 
-const SIGNUP_URL = 'http://127.0.0.1:5000/signup';
+const SIGNUP_URL = 'https://vitalcare-i3og.onrender.com/signup';
 
 // --- Event Listener for Form Submission ---
 signupForm.addEventListener('submit', async (event) => {
